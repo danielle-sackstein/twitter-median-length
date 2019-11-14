b563701034712fe70d131554e9aef383ea3f0608
danielle-sackstein <danielle.sackstein94@gmail.com>
2019-11-14 17:22:00 +0200

    Initial commit

---
+++ src/main/java/home/assignement/MainApplication.java
@@ -0,0 +1,11 @@
+package home.assignement;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
